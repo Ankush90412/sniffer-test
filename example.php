663660d430c68c2d310eca01d840119e5fdcc294
Tru Team <51973101+tru-team@users.noreply.github.com>
2024-05-10 16:31:33 +0530

    pipeline

--- example.php
+++ example.php
@@ -1,12 +1,8 @@
 <?php
-
-// This is a sample PHP file with intentional coding standard violations
-
 class ExampleClass {
     public function helloWorld() {
         echo "Hello World!";
     }
 }
-
 $exampleObject = new ExampleClass();
 $exampleObject->helloWorld();
