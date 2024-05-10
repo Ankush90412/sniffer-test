31b7b4df3801a61d0af637fc114660d5a763a830
Ankush90412 <163531116+Ankush90412@users.noreply.github.com>
2024-05-10 16:19:29 +0530

    Create ekample.php

---
+++ ekample.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is a sample PHP file with intentional coding standard violations
+
+class ExampleClass {
+    public function helloWorld() {
+        echo "Hello World!";
+    }
+}
+
+$exampleObject = new ExampleClass();
+$exampleObject->helloWorld();
